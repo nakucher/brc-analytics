6076fffbad89c357835d00258450d01081c1a33c
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-02 09:05:22 +1000

    feat: added section border (#6)

--- website/app/components/Layout/components/Main/main.styles.ts
+++ website/app/components/Layout/components/Main/main.styles.ts
@@ -1,6 +1,9 @@
 import { Main as DXMain } from "@databiosphere/findable-ui/lib/components/Layout/components/Main/main";
+import { smokeMain } from "@databiosphere/findable-ui/lib/styles/common/mixins/colors";
 import styled from "@emotion/styled";
 
 export const StyledMain = styled(DXMain)`
+  background-color: ${smokeMain};
   flex-direction: column;
+  gap: 1px;
 `;
