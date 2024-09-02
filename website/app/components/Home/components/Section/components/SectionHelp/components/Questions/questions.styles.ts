d32355ae5f024de52befcf603dc9b8e4b3edc130
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-02 14:09:22 +1000

    feat: add section help and questions (#6)

---
+++ website/app/components/Home/components/Section/components/SectionHelp/components/Questions/questions.styles.ts
@@ -0,0 +1,55 @@
+import { mediaTabletUp } from "@databiosphere/findable-ui/lib/styles/common/mixins/breakpoints";
+import { inkLight } from "@databiosphere/findable-ui/lib/styles/common/mixins/colors";
+import {
+  textBodyLarge4002Lines,
+  textBodyLarge500,
+} from "@databiosphere/findable-ui/lib/styles/common/mixins/fonts";
+import { elevation01 } from "@databiosphere/findable-ui/lib/theme/common/shadows";
+import styled from "@emotion/styled";
+import { Accordion as MAccordion } from "@mui/material";
+import { sectionGrid } from "../../../../section.styles";
+
+export const Grid = styled.div`
+  ${sectionGrid};
+  grid-column: 1 / -1;
+  grid-template-columns: 1fr;
+
+  ${mediaTabletUp} {
+    grid-column: 6 / -1;
+    grid-template-columns: repeat(7, 1fr);
+  }
+`;
+
+export const StyledAccordion = styled(MAccordion)`
+  box-shadow: ${elevation01} !important;
+  display: grid;
+  grid-column: 1 / -1;
+  padding: 12px 0;
+
+  .MuiAccordionSummary-root {
+    flex-direction: row;
+    min-height: 0;
+    padding: 8px 20px;
+
+    .MuiAccordionSummary-content {
+      ${textBodyLarge500};
+      margin: 0;
+    }
+  }
+
+  .MuiAccordionDetails-root {
+    ${textBodyLarge4002Lines};
+    color: ${inkLight};
+    margin: 0;
+    padding: 0 20px 8px;
+
+    .MuiLink-root {
+      color: #28285b;
+      text-decoration: underline;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+` as typeof MAccordion;
---
+++ website/app/components/Home/components/Section/components/SectionHelp/sectionHelp.styles.ts
@@ -0,0 +1,48 @@
+import { mediaTabletUp } from "@databiosphere/findable-ui/lib/styles/common/mixins/breakpoints";
+import { white } from "@databiosphere/findable-ui/lib/styles/common/mixins/colors";
+import styled from "@emotion/styled";
+import {
+  Section,
+  sectionGrid,
+  SectionHeadline,
+  SectionLayout as DefaultLayout,
+  SectionSubtitle,
+  SectionTitle,
+} from "../../section.styles";
+
+export const StyledSection = styled(Section)`
+  background-color: ${white};
+`;
+
+export const SectionLayout = styled(DefaultLayout)`
+  ${sectionGrid};
+  padding: 85px 16px 150px;
+`;
+
+export const Headline = styled(SectionHeadline)`
+  align-content: flex-start;
+  grid-column: 1 / -1;
+  max-width: 504px;
+
+  ${mediaTabletUp} {
+    grid-column: 1 / 5;
+    max-width: unset;
+  }
+`;
+
+export const StyledSectionTitle = styled(SectionTitle)`
+  max-width: 276px;
+`;
+
+export const StyledSectionSubTitle = styled(SectionSubtitle)`
+  line-height: 28px;
+
+  .MuiLink-root {
+    color: #28285b;
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`;
